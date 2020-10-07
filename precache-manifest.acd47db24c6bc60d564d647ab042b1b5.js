41b89328dfd5588381e6fef97cb333ffbcafb832
Michael Zlotkowski <michael.zlotkowski@gmail.com>
2020-10-07 11:26:26 -0700

    Updates

---
+++ precache-manifest.acd47db24c6bc60d564d647ab042b1b5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec8398238228b94ad9b64fc4cb11fbaf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "748d78e057eebb5a3eb7",
+    "url": "/static/css/main.99aab9b5.chunk.css"
+  },
+  {
+    "revision": "445dcac4b0ea527300d0",
+    "url": "/static/js/2.570ffbaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.570ffbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748d78e057eebb5a3eb7",
+    "url": "/static/js/main.4af4a970.chunk.js"
+  },
+  {
+    "revision": "c03613ae30fec6fc4efa",
+    "url": "/static/js/runtime-main.e7bfbf6c.js"
+  },
+  {
+    "revision": "9b2ba273c96b82daf49792d61b5e2dea",
+    "url": "/static/media/EM.9b2ba273.png"
+  },
+  {
+    "revision": "03693a4790d506bb851b36a4b9eb2151",
+    "url": "/static/media/FL.03693a47.png"
+  },
+  {
+    "revision": "16d4d6719f9bffcc64c891ae85cf6dd4",
+    "url": "/static/media/SCM.16d4d671.png"
+  },
+  {
+    "revision": "527a3fc2340bed86bfbf104986750d96",
+    "url": "/static/media/SixSigma.527a3fc2.png"
+  },
+  {
+    "revision": "fa3731ca81cdec5ff60355e757662a04",
+    "url": "/static/media/header-bg.fa3731ca.jpg"
+  },
+  {
+    "revision": "f40ed5b1e791e9d2479ba8b5e67f2df8",
+    "url": "/static/media/map-image.f40ed5b1.png"
+  },
+  {
+    "revision": "872d8e803fa9f698cdafa6483c212795",
+    "url": "/static/media/white_logo.872d8e80.png"
+  }
+]);
